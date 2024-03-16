f33641f857134e43d04dc06cb8965be488076331
ibrahim_ahmad <ibrahimahmad.info@gmail.com>
2024-03-16 08:52:29 +0800

    Add function and button to add book object into library array.

--- object.js
+++ object.js
@@ -1,13 +1,24 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read){
-    this.title = title;
+    this.title  = title;
     this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.text = this.title      + ', ' 
-                + this.author   + ', '
-                + this.pages    + ', '
-                + this.read     + '.';
-    this.info = () => {
-        return this.text;
-    };
-}
+    this.pages  = pages;
+    this.read   = read;
+};
+
+// temporary user input
+const title = 'A Field Guide to Lies';
+const author = 'Daniel K. Levitin';
+const pages = 292;
+const read = 'In Progress';
+
+// add input to object
+let book = new Book(title, author, pages, read);
+
+// add book object to library array
+addBookLibrary = () => {myLibrary.push(book)};
+
+// add event listener
+const addButton = document.querySelector(".addBook");
+addButton.addEventListener('click', addBookLibrary);
